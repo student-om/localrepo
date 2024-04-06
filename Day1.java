8f4b930c0b0c555c71a3c35f5f69d7dff34f703c
student-om <155445191+student-om@users.noreply.github.com>
2024-04-06 23:46:04 +0530

    Day1

---
+++ Day1.java
@@ -0,0 +1,25 @@
+class Day1{
+	public static void main(String args[]){
+	try{
+	Thread.sleep(1000);
+	System.out.println("inside try");
+	Thread.sleep(1000);
+	System.out.println(args[0]);
+	Thread.sleep(1000);
+	System.out.println("Inside TRy2");
+	}
+	
+	//catch(Exception e) ---> error will come(catch should be written at last)
+	catch(ArrayIndexOutOfBoundsException e){   // this is a run time exception
+	System.out.println("wrong index");
+	}
+	//2 ArrayIndexOutOfBoundsException ARE also not allowed
+	catch(InterruptedException e){  //this is a compile time exception
+	System.out.println("InterruptedException found");
+	}
+	catch(Exception e) {
+	System.out.println("Exce[tion found");
+	}
+	
+	}
+}
