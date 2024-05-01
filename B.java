be50da3b1735f06670008cc1840b1c3577e92302
student-om <2022bit046@sggs.ac.in>
2024-05-01 22:04:41 +0530

    Packages in java

--- B.java
+++ B.java
@@ -1,6 +1,6 @@
 //classes A and B are in the same package. if B is in same package then it can access members of A directly(Except private) 
 //so B.class will not created in (package same) --> error comes at line 9(can't access private member)
-
+//.
 package same;
 import same.A;
 public class B{
